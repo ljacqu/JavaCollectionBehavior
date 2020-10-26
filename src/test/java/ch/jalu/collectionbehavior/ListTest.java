786fd2a65a31a9590b51b6869af9766b3fec896b
ljacqu <ljacqu@users.noreply.github.com>
2020-10-26 20:23:33 +0100

    Add test/docs for List and Set variants

---
+++ src/test/java/ch/jalu/collectionbehavior/ListTest.java
@@ -0,0 +1,219 @@
+package ch.jalu.collectionbehavior;
+
+import com.google.common.collect.ImmutableList;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.RandomAccess;
+
+import static com.google.common.collect.Lists.newArrayList;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.arrayContaining;
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.instanceOf;
+import static org.hamcrest.Matchers.not;
+import static org.hamcrest.Matchers.sameInstance;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class ListTest {
+
+    /**
+     * ArrayList: standard mutable List implementation. Fully supports null.
+     */
+    @Test
+    void testJdkArrayList() {
+        List<String> list = new ArrayList<>();
+        list.add("a");
+        list.add("b");
+        list.add("c");
+
+        assertThat(list, contains("a", "b", "c"));
+        assertThat(list, instanceOf(RandomAccess.class));
+
+        assertThat(list.contains(null), equalTo(false));
+        list.add(null);
+        assertThat(list.contains(null), equalTo(true));
+    }
+
+    /**
+     * {@link List#of} produces a fully immutable list that does not support null,
+     * even when null is called on {@link List#contains}. Copies the incoming array.
+     */
+    @Test
+    void testJdkListOf() {
+        String[] elements = { "a", "b", "c", "d" };
+        List<String> list = List.of(elements);
+
+        elements[2] = "changed";
+        assertThat(list, contains("a", "b", "c", "d"));
+        assertThat(list, instanceOf(RandomAccess.class));
+
+        assertThrows(UnsupportedOperationException.class, () -> list.add("foo"));
+        assertThrows(UnsupportedOperationException.class, () -> list.remove(3));
+        assertThrows(UnsupportedOperationException.class, () -> list.set(3, "foo"));
+
+        assertThrows(NullPointerException.class, () -> List.of("a", null, "c"));
+        assertThrows(NullPointerException.class, () -> list.contains(null));
+    }
+
+    /**
+     * {@link List#copyOf} copies the incoming collection and returns a fully immutable List;
+     * recognizes lists of its own class and returns the same instance instead of unnecessarily copying.
+     * Does not support null (not even as argument passed into things like {@link List#contains}).
+     */
+    @Test
+    void testJdkListCopyOf() {
+        List<String> elements = new ArrayList<>(Arrays.asList("a", "b", "c", "d"));
+        List<String> list = List.copyOf(elements);
+
+        elements.set(2, "changed");
+        assertThat(list, contains("a", "b", "c", "d"));
+        assertThat(list, instanceOf(RandomAccess.class));
+
+        assertThat(List.copyOf(list), sameInstance(list));
+
+        assertThrows(UnsupportedOperationException.class, () -> list.add("foo"));
+        assertThrows(UnsupportedOperationException.class, () -> list.remove(3));
+        assertThrows(UnsupportedOperationException.class, () -> list.set(3, "foo"));
+
+        List<String> listWithNull = Arrays.asList("a", null, "c");
+        assertThrows(NullPointerException.class, () -> List.copyOf(listWithNull));
+        assertThrows(NullPointerException.class, () -> list.contains(null));
+    }
+
+    /**
+     * {@link Arrays#asList} wraps an array into the List interface. Allows to change individual elements
+     * but elements cannot be added or removed. The array passed into it is not copied! Supports null.
+     */
+    @Test
+    void testJdkArraysAsList() {
+        String[] elements = { "a", "b", "c", "d" };
+        List<String> list = Arrays.asList(elements);
+
+        elements[2] = "changed";
+        assertThat(list, contains("a", "b", "changed", "d"));
+        assertThat(list, instanceOf(RandomAccess.class));
+
+        assertThrows(UnsupportedOperationException.class, () -> list.add("foo"));
+        assertThrows(UnsupportedOperationException.class, () -> list.remove(3));
+        list.set(2, "foo");
+        assertThat(list, contains("a", "b", "foo", "d"));
+        assertThat(elements, arrayContaining("a", "b", "foo", "d")); // backing array changed via list
+
+        assertThat(Arrays.asList("a", null, "c"), contains("a", null, "c"));
+        assertThat(list.contains(null), equalTo(false));
+    }
+
+    /**
+     * {@link ImmutableList#copyOf} returns a fully immutable list. Does not support null as element but
+     * null can be passed as argument into methods like {@link List#contains}. Copies the incoming array.
+     * Prefer {@link ImmutableList#of} if you are not starting from an array (unlike this test case).
+     */
+    @Test
+    void testGuavaImmutableList() {
+        String[] elements = { "a", "b", "c", "d" };
+        List<String> list = ImmutableList.copyOf(elements);
+
+        elements[2] = "changed";
+        assertThat(list, contains("a", "b", "c", "d"));
+        assertThat(list, instanceOf(RandomAccess.class));
+
+        assertThrows(UnsupportedOperationException.class, () -> list.add("foo"));
+        assertThrows(UnsupportedOperationException.class, () -> list.remove(3));
+        assertThrows(UnsupportedOperationException.class, () -> list.set(3, "foo"));
+
+        assertThrows(NullPointerException.class, () -> ImmutableList.of("a", null, "c"));
+        assertThat(list.contains(null), equalTo(false));
+    }
+
+    /**
+     * {@link ImmutableList#copyOf(Iterable)} copies the incoming collection and produces a fully immutable
+     * List. Recognizes instances of the same class and avoids unnecessary copies. Does not support null as
+     * element but accepts null passed as argument into {@link List#contains} etc.
+     */
+    @Test
+    void testGuavaImmutableListCopy() {
+        List<String> elements = newArrayList("a", "b", "c", "d");
+        List<String> list = ImmutableList.copyOf(elements);
+
+        elements.set(2, "changed");
+        assertThat(list, contains("a", "b", "c", "d"));
+        assertThat(list, instanceOf(RandomAccess.class));
+
+        assertThat(ImmutableList.copyOf(list), sameInstance(list));
+
+        assertThrows(UnsupportedOperationException.class, () -> list.add("foo"));
+        assertThrows(UnsupportedOperationException.class, () -> list.remove(3));
+        assertThrows(UnsupportedOperationException.class, () -> list.set(3, "foo"));
+
+        List<String> elementsWithNull = newArrayList("a", null, "c");
+        assertThrows(NullPointerException.class, () -> ImmutableList.copyOf(elementsWithNull));
+        assertThat(list.contains(null), equalTo(false));
+    }
+
+    /**
+     * {@link Collections#emptyList()} returns an immutable empty list. Always the same instance.
+     * Nice readable name for when an empty list is desired to be returned.
+     */
+    @Test
+    void testJdkCollectionsEmptyList() {
+        List<String> list1 = Collections.emptyList();
+        List<Integer> list2 = Collections.emptyList();
+
+        assertThat(list1, sameInstance(list2));
+        assertThat(list1, instanceOf(RandomAccess.class));
+
+        assertThat(list1.contains("foo"), equalTo(false));
+        assertThat(list1.contains(null), equalTo(false));
+
+        assertThrows(UnsupportedOperationException.class, () -> list1.add("f"));
+    }
+
+    /**
+     * {@link Collections#unmodifiableList} wraps a List and does not copy its elements. Changes to the
+     * original List are propagated but cannot be modified via the returned list itself. Fully supports null
+     * as elements or method parameter.
+     * Implements RandomAccess only if the underlying List implements it.
+     */
+    @Test
+    void testJdkCollectionsUnmodifiableList() {
+        List<String> elements = newArrayList("a", "b", "c", "d");
+        List<String> list = Collections.unmodifiableList(elements);
+
+        elements.set(2, "changed");
+        assertThat(list, contains("a", "b", "changed", "d"));
+        assertThat(list, instanceOf(RandomAccess.class)); // Because wrapped list is RandomAccess, too.
+
+        assertThat(Collections.unmodifiableList(list), not(sameInstance(list)));
+
+        assertThrows(UnsupportedOperationException.class, () -> list.add("foo"));
+        assertThrows(UnsupportedOperationException.class, () -> list.remove(3));
+        assertThrows(UnsupportedOperationException.class, () -> list.set(3, "foo"));
+
+        List<String> listWithNull = Arrays.asList("a", null, "c");
+        assertThat(Collections.unmodifiableList(listWithNull), contains("a", null, "c"));
+        assertThat(list.contains(null), equalTo(false));
+    }
+
+    /**
+     * {@link Collections#singletonList} provides a list with a single given element. Fully immutable. Supports null.
+     */
+    @Test
+    void testJdkCollectionsSingletonList() {
+        List<String> list = Collections.singletonList("test");
+
+        assertThat(list, contains("test"));
+        assertThat(list, instanceOf(RandomAccess.class));
+
+        assertThrows(UnsupportedOperationException.class, () -> list.add("foo"));
+        assertThrows(UnsupportedOperationException.class, () -> list.remove(0));
+        assertThrows(UnsupportedOperationException.class, () -> list.set(0, "foo"));
+
+        assertThat(Collections.singletonList(null), contains((Object) null));
+        assertThat(list.contains(null), equalTo(false));
+    }
+}
---
+++ src/test/java/ch/jalu/collectionbehavior/SetTest.java
@@ -0,0 +1,210 @@
+package ch.jalu.collectionbehavior;
+
+import com.google.common.collect.ImmutableSet;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Set;
+
+import static com.google.common.collect.Lists.newArrayList;
+import static com.google.common.collect.Sets.newHashSet;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.containsInAnyOrder;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.not;
+import static org.hamcrest.Matchers.sameInstance;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class SetTest {
+
+    /**
+     * {@link HashSet}: standard mutable Set. Fully supports null. Does not keep insertion order.
+     * Elements already added to it are silently ignored.
+     */
+    @Test
+    void testJdkHashSet() {
+        Set<Integer> set = new HashSet<>(Arrays.asList(1, 4, 9, 16, 9));
+
+        assertContainsButNotInOrder(set, 1, 4, 9, 16);
+
+        assertThat(set.contains(null), equalTo(false));
+        set.add(null);
+        assertThat(set.contains(null), equalTo(true));
+    }
+
+    /**
+     * {@link LinkedHashSet}: mutable Set that keeps insertion order. Fully supports null.
+     * Elements already added to it are silently ignored.
+     */
+    @Test
+    void testJdkLinkedHashSet() {
+        Set<Integer> set = new LinkedHashSet<>(Arrays.asList(1, 4, 9, 16, 9));
+
+        assertThat(set, contains(1, 4, 9, 16));
+
+        assertThat(set.contains(null), equalTo(false));
+        set.add(null);
+        assertThat(set.contains(null), equalTo(true));
+    }
+
+    /**
+     * {@link Set#of} produces a fully immutable Set. Does not support null (not even for {@link Set#contains} etc.).
+     * Throws an exception if any element is passed in twice. Random iteration order.
+     */
+    @Test
+    void testJdkSetOf() {
+        Integer[] elements = { 1, 4, 9, 16 };
+        Set<Integer> set = Set.of(elements);
+
+        elements[2] = -999;
+        assertContainsButNotInOrder(set, 1, 4, 9, 16);
+
+        assertThrows(UnsupportedOperationException.class, () -> set.add(777));
+        assertThrows(UnsupportedOperationException.class, () -> set.remove(3));
+
+        assertThrows(NullPointerException.class, () -> Set.of(14, null, 16));
+        assertThrows(NullPointerException.class, () -> set.contains(null));
+
+        assertThrows(IllegalArgumentException.class, () -> Set.of(14, 15, 14));
+    }
+
+    /**
+     * {@link Set#copyOf} produces a fully immutable Set. Does not support null (not even for {@link Set#contains} etc.).
+     * Duplicate elements in the original collection are ignored. The collection passed in is copied.
+     * Random iteration order.
+     */
+    @Test
+    void testJdkSetCopyOf() {
+        List<Integer> elements = newArrayList(1, 4, 9, 16, 9);
+        Set<Integer> set = Set.copyOf(elements);
+
+        elements.remove(4);
+        assertContainsButNotInOrder(set, 1, 4, 9, 16);
+
+        assertThat(Set.copyOf(set), sameInstance(set));
+
+        assertThrows(UnsupportedOperationException.class, () -> set.add(777));
+        assertThrows(UnsupportedOperationException.class, () -> set.remove(3));
+
+        assertThrows(NullPointerException.class, () -> set.contains(null));
+
+        List<Integer> elementsWithNull = Arrays.asList(1, 4, null, 16);
+        assertThrows(NullPointerException.class, () -> Set.copyOf(elementsWithNull));
+    }
+
+    /**
+     * {@link ImmutableSet#of} produces a fully immutable Set. Does not support null as elements.
+     * Insertion order is kept. Can be instantiated with duplicates
+     * (also when using the builder {@link ImmutableSet#builder()}).
+     */
+    @Test
+    void testGuavaImmutableSet() {
+        Integer[] elements = { 1, 4, 9, 16, 9 };
+        Set<Integer> set = ImmutableSet.copyOf(elements);
+
+        elements[2] = -999;
+        assertThat(set, contains(1, 4, 9, 16));
+
+        assertThrows(UnsupportedOperationException.class, () -> set.add(777));
+        assertThrows(UnsupportedOperationException.class, () -> set.remove(3));
+
+        assertThrows(NullPointerException.class, () -> ImmutableSet.of(14, null, 16));
+        assertThat(set.contains(null), equalTo(false));
+
+        assertThat(ImmutableSet.of(14, 15, 14), contains(14, 15));
+        assertThat(ImmutableSet.builder()
+            .add(14)
+            .add(15, 15)
+            .add(14)
+            .add(16)
+            .build(), contains(14, 15, 16));
+    }
+
+    /**
+     * {@link ImmutableSet#copyOf} produces a fully immutable Set. Null as element is not supported but can be used
+     * in {@link Set#contains} etc. Elements are copied. Retains iteration order of the original collection.
+     * Recognizes its own instances and avoids unnecessary copies.
+     */
+    @Test
+    void testGuavaImmutableSetCopy() {
+        List<Integer> elements = newArrayList(1, 4, 9, 16, 9);
+        Set<Integer> set = ImmutableSet.copyOf(elements);
+
+        elements.set(2, -999);
+        assertThat(set, contains(1, 4, 9, 16));
+
+        assertThat(ImmutableSet.copyOf(set), sameInstance(set));
+
+        assertThrows(UnsupportedOperationException.class, () -> set.add(888));
+        assertThrows(UnsupportedOperationException.class, () -> set.remove(9));
+
+        List<Integer> elementsWithNull = newArrayList(1, null, 9);
+        assertThrows(NullPointerException.class, () -> ImmutableSet.copyOf(elementsWithNull));
+        assertThat(set.contains(null), equalTo(false));
+    }
+
+    /**
+     * {@link Collections#emptySet()} always returns the same instance: immutable empty Set.
+     */
+    @Test
+    void testJdkCollectionsEmptySet() {
+        Set<String> set1 = Collections.emptySet();
+        Set<Integer> set2 = Collections.emptySet();
+
+        assertThat(set1, sameInstance(set2));
+
+        assertThat(set1.contains("foo"), equalTo(false));
+        assertThat(set1.contains(null), equalTo(false));
+
+        assertThrows(UnsupportedOperationException.class, () -> set1.add("f"));
+    }
+
+    /**
+     * {@link Collections#unmodifiableSet} wraps a Set into an immutable Set facade. Changes to the backing
+     * collection are reflected. Supports null as elements. Iteration order kept from underlying collection.
+     */
+    @Test
+    void testJdkCollectionsUnmodifiableSet() {
+        Set<Integer> elements = new LinkedHashSet<>(Arrays.asList(1, 4, 9, 16));
+        Set<Integer> set = Collections.unmodifiableSet(elements);
+
+        elements.remove(4);
+        assertThat(set, contains(1, 9, 16));
+
+        assertThat(Collections.unmodifiableSet(set), not(sameInstance(set)));
+
+        assertThrows(UnsupportedOperationException.class, () -> set.add(777));
+        assertThrows(UnsupportedOperationException.class, () -> set.remove(1));
+
+        Set<Integer> setWithNull = newHashSet(1, null, 9);
+        assertThat(Collections.unmodifiableSet(setWithNull), containsInAnyOrder(1, null, 9));
+        assertThat(set.contains(null), equalTo(false));
+    }
+
+    /**
+     * {@link Collections#singleton} produces an immutable Set with a single element. Supports null.
+     */
+    @Test
+    void testJdkCollectionsSingleton() {
+        Set<Integer> set = Collections.singleton(19);
+
+        assertThat(set, contains(19));
+
+        assertThrows(UnsupportedOperationException.class, () -> set.add(22));
+        assertThrows(UnsupportedOperationException.class, () -> set.remove(19));
+
+        assertThat(Collections.singleton(null), contains((Object) null));
+        assertThat(set.contains(null), equalTo(false));
+    }
+
+    private static void assertContainsButNotInOrder(Set<Integer> set, Integer... values) {
+        assertThat(set, containsInAnyOrder(values));
+        assertThat(set, not(contains(values)));
+        // Of course it's still possible that by coincidence the Set has the same order, but we just have to live with this
+    }
+}
